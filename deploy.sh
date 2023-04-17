e087541a8787a54981201b95a9d12bee0c241a52
RyanRongY <yarong@ucsd.edu>
2023-04-17 01:06:58 +0000

    hh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-LOG_FILE="/var/www/ryanrong.site/deploy_log.txt"
-
-echo "Deploying changes..." >> $LOG_FILE
+echo "Deploying changes..." >> /var/www/ryanrong.site/public_html/deploy_log.txt
 
 # Navigate to the root directory of your website
 cd /var/www/ryanrong.site/public_html
 
-# Fetch the latest changes from the remote repository and reset the local branch
-git fetch origin
-git reset --hard origin/master
+# Pull in the latest changes from the GitHub repository
+git pull --verbose >> /var/www/ryanrong.site/public_html/deploy_log.txt 2>&1
 
-echo "Deployment complete!" >> $LOG_FILE
+echo "Deployment complete!" >> /var/www/ryanrong.site/public_html/deploy_log.txt
